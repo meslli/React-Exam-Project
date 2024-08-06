e67474e73ca94b2f7ceaf431347acf8614b87c9e
mesli <you@example.com>
2024-08-06 14:33:49 +0300

    Add routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import CatalogueGames from './components/catalogue-games/CatalogueGame'
+import DetailsGame from './components/details-game/DetailsGame'
+import CreateGame from './components/create-game/CreateGame'
+import EditGame from './components/edit-game/EditGame'
 
 function App() {
   return (
@@ -7,7 +15,15 @@ function App() {
       <Header />
 
       <main id="main-content">
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/catalogue-games' element={<CatalogueGames />} />
+          <Route path='/details-game' element={<DetailsGame />} />
+          <Route path='/create-game' element={<CreateGame />} />
+          <Route path='/edit-game' element={<EditGame />} />
+        </Routes>
       </main>
     </div>
   )
