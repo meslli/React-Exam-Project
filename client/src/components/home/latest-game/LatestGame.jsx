57e489bf8cc9e9d3f12b821237dc7e99045e6735
mesli <you@example.com>
2024-08-06 16:50:38 +0300

    Add latest games request and component

---
+++ client/src/components/home/latest-game/LatestGame.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+const LatestGame = ({ _id, imgUrl, title }) => {
+    return (
+        <div className="game">
+            <div className="image-wrap">
+                <img src={imgUrl} />
+            </div>
+
+            <h3>{title}</h3>
+            <div className="rating">
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+            </div>
+
+            <div className="data-buttons">
+                <Link 
+                    to={`/details-game/${_id}`} 
+                    className="btn details-btn"
+                >
+                    Details
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default LatestGame
