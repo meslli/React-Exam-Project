f04668308c4d1e6fb6ae9446f8b746486cabb594
mesli <you@example.com>
2024-08-07 16:42:00 +0300

    User and guest authenticated logic

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,33 @@
+import { useContext, useReducer} from "react"
+
 import { Link } from "react-router-dom"
 
+import { UserContext } from "../../context/UserContext"
+
 const Header = () => {
+    const { isAuthenticated } = useContext(UserContext)
+
     return (
         <header>
-            {/* <!-- Navigation --> */}
             <h1><Link className="home" to="/">GamesPlay</Link></h1>
 
             <nav>
                 <Link to="/catalogue-games">All games</Link>
-                {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <Link to="/create-game">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                {/* <!-- Guest users --> */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+
+                {isAuthenticated 
+                    ? (
+                        <div id="user">
+                            <Link to="/create-game">Create Game</Link>
+                            <Link to="/logout">Logout</Link>
+                        </div>
+                    )
+                    : (
+                        <div id="guest">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                    )
+                }       
             </nav>
         </header>
     )
