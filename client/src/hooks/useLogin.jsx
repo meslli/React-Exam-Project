6ee2c77a22b17d6ca440ffe232da0bc6595f7651
mesli <you@example.com>
2024-08-07 14:52:59 +0300

    Add navigate to useLogin

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 import { loginUser } from "../api/users-api"
 
-export const useLogin = () => {
+export const useLogin = () => {            
+    const navigate = useNavigate()
+
     const login = async (values) => {
         try {
             const result = await loginUser(values)
 
             console.log(result)
+            navigate('/')
         } catch(err) {
             console.error(err.message)
         }
